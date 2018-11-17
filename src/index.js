141f2e1e48eff978c0f5a4e6ccff4a9fce724356
chiiia12 <llnsbchsep12@gmail.com>
2018-11-17 15:09:31 +0900

    create base action/reducers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import App from './components/App';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import todo from './reducers';
+import {addTodo} from './actions';
+
+let store = createStore(todo)
+store.dispatch(addTodo('Hello world'))
+console.log(store.getState())
 
 render(
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>,
     document.getElementById('root')
