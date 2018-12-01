ab063090df8e65fe140808ca3d38871810993c45
chiiia12 <llnsbchsep12@gmail.com>
2018-12-01 17:23:36 +0900

    add empty view

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -3,19 +3,28 @@ import PropTypes from 'prop-types';
 import History from './History'
 import List from '@material-ui/core/List';
 
-const HistoryList= ({historys})=>(
-    <List>
-        {historys.map((history)=>
-       <History
-       key={history.id}
-       {...history}/> 
-        )}
-    </List>
-)
-HistoryList.propTypes={
-    historys:PropTypes.arrayOf(PropTypes.shape({
-        id:PropTypes.number.isRequired,
-        test:PropTypes.string.isRequired,
+const HistoryList = ({ historys }) => {
+    if (historys.size === 0) {
+        return (
+            <p>There are no issues that you saw recently.</p>
+        )
+    } else {
+        return (
+            <List>
+                {historys.map((history) =>
+                    <History
+                        key={history.id}
+                        {...history} />
+                )}
+            </List>
+        )
+
+    }
+}
+HistoryList.propTypes = {
+    historys: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        test: PropTypes.string.isRequired,
     }).isRequired).isRequired
 }
 export default HistoryList 
