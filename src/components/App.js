3640be27d18637f0170fdefb3d8c095f15323347
chiiia12 <llnsbchsep12@gmail.com>
2018-12-03 18:57:51 +0900

    add AppBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import VisibleHistoryList from '../containers/VisibleHistoryList'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 
 const App = () => (
   <div>
-    <h1>Recent Github Issues</h1>
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          Recent Github Issues
+          </Typography>
+      </Toolbar>
+    </AppBar>
     <VisibleHistoryList />
   </div>
 )
